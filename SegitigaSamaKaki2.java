4e6827c81683317278acf3d92459f0e05f082d21
ZahirJauza3355 <muhammadjauza55@gmail.com>
2025-09-26 07:58:11 +0700

    SegitigaSamaKaki2.java

--- SegitigaSamaKaki2.java
+++ SegitigaSamaKaki2.java
@@ -2,22 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package overriding;
+package Overloading;
 
 /**
  *
  * @author jauza
  */
 public class SegitigaSamaKaki extends BangunDatar{
-    public double alas,tinggi;
-    public SegitigaSamaKaki(double alas,double tinggi){
+   public double alas,tinggi;
+    public void hitungLuas(){
+        luas = 0.5*alas*tinggi;
+    }
+    public void hitungluas(double alas,double tinggi){
         this.alas = alas;
         this.tinggi = tinggi;
-    }
-    public void hitungluas(){
         luas = 0.5*alas*tinggi;
     }
     public void hitungkeliling(){
+        double simir = Math.sqrt((0.5*alas*0.5*alas)+(tinggi*tinggi));
+        keliling = (2*simir)+alas;
+    }
+    public void hitungKeliling(double alas,double tinggi){
+        this.alas = alas;
+        this.tinggi = tinggi;
         double simir = Math.sqrt((0.5*alas*0.5*alas)+(tinggi*tinggi));
         keliling = (2*simir)+alas;
     }
@@ -26,5 +33,5 @@ public class SegitigaSamaKaki extends BangunDatar{
         System.out.println("TINGGi ="+tinggi);
         super.tampilData();
     }
-    
 }
+
